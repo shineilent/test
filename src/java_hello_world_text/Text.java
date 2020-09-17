5289cf1a417100ea44002f0a2f87dff3061a1780
shineilent <3141449045@qq.com>
2020-09-17 16:52:52 +0800

    test

---
+++ src/java_hello_world_text/Text.java
@@ -0,0 +1,4 @@
+package java_hello_world_text;
+
+public class Text {
+}
---
+++ src/java_hello_world_text/hello.java
@@ -0,0 +1,12 @@
+package java_hello_world_text;
+
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello,World!");
+
+
+
+    }
+}
